ab107c21c18a5ca10607f23fc578b095a96f9b07
westor <westor7@gmail.com>
2025-02-06 02:02:00 +0200

    Upload project files

---
+++ scripts/moreMilk.lua
@@ -0,0 +1,195 @@
+-- Author: westor
+-- Contact: westor7 @ Discord
+--
+-- Copyright (c) 2025 westor
+
+moreMilk = {}
+moreMilk.settings = {}
+moreMilk.name = g_currentModName or "FS25_moreMilk"
+moreMilk.version = "1.0.0.0"
+moreMilk.dir = g_currentModDirectory
+moreMilk.init = false
+
+function moreMilk.prerequisitesPresent(specializations)
+    return true
+end
+
+function moreMilk:loadMap()
+	if g_dedicatedServer or g_currentMission.missionDynamicInfo.isMultiplayer or not g_server or not g_currentMission:getIsServer() then
+		Logging.error("[%s]: Error, Cannot use this mod because this mod is working only for singleplayer!", moreMilk.name)
+
+		return
+    end
+
+	InGameMenu.onMenuOpened = Utils.appendedFunction(InGameMenu.onMenuOpened, moreMilk.initUi)
+
+	FSBaseMission.saveSavegame = Utils.appendedFunction(FSBaseMission.saveSavegame, moreMilk.saveSettings)
+end
+
+function moreMilk:defSettings()
+	moreMilk.settings.Multiplier = 2
+	moreMilk.settings.Multiplier_OLD = 2
+end
+
+function moreMilk:saveSettings()
+	Logging.info("[%s]: Trying to save settings..", moreMilk.name)
+
+	local modSettingsDir = getUserProfileAppPath() .. "modSettings"
+	local fileName = "moreMilk.xml"
+	local createXmlFile = modSettingsDir .. "/" .. fileName
+
+	local xmlFile = createXMLFile("moreMilk", createXmlFile, "moreMilk")
+	
+	setXMLFloat(xmlFile, "moreMilk.milk#Multiplier",moreMilk.settings.Multiplier)
+	
+	saveXMLFile(xmlFile)
+	delete(xmlFile)
+	
+	Logging.info("[%s]: Settings have been saved.", moreMilk.name)
+end
+
+function moreMilk:loadSettings()
+	Logging.info("[%s]: Trying to load settings..", moreMilk.name)
+	
+	local modSettingsDir = getUserProfileAppPath() .. "modSettings"
+	local fileName = "moreMilk.xml"
+	local fileNamePath = modSettingsDir .. "/" .. fileName
+	
+	if fileExists(fileNamePath) then
+		Logging.info("[%s]: File founded, loading now the settings..", moreMilk.name)
+		
+		local xmlFile = loadXMLFile("moreMilk", fileNamePath)
+		
+		if xmlFile == 0 then
+			Logging.warning("[%s]: Could not read the data from XML file, maybe the XML file is empty or corrupted, using the default!", moreMilk.name)
+			
+			moreMilk:defSettings()
+			
+			Logging.info("[%s]: Settings have been loaded.", moreMilk.name)
+			
+			return
+		end
+
+		local Multiplier = getXMLFloat(xmlFile, "moreMilk.milk#Multiplier")
+
+		if Multiplier == nil or Multiplier == 0 then
+			Logging.warning("[%s]: Could not parse the correct 'Multiplier' value from the XML file, maybe it is corrupted, using the default!", moreMilk.name)
+			
+			Multiplier = 2
+		end
+
+		if Multiplier < 1.5 then
+			Logging.warning("[%s]: Could not retrieve the correct 'Multiplier' digital number value because it is lower than '1.5' from the XML file or it is corrupted, using the default!", moreMilk.name)
+			
+			Multiplier = 2
+		end
+		
+		if Multiplier > 100 then
+			Logging.warning("[%s]: Could not retrieve the correct 'Multiplier' digital number value because it is higher than '100' from the XML file or it is corrupted, using the default!", moreMilk.name)
+			
+			Multiplier = 2
+		end
+		
+		moreMilk.settings.Multiplier = Multiplier
+		moreMilk.settings.Multiplier_OLD = Multiplier
+		
+		delete(xmlFile)
+					
+		Logging.info("[%s]: Settings have been loaded.", moreMilk.name)
+	else
+		moreMilk:defSettings()
+
+		Logging.info("[%s]: NOT any File founded!, using the default settings.", moreMilk.name)
+	end
+end
+
+function moreMilk:initUi()
+	if not moreMilk.init then
+		local uiSettingsmoreMilk = moreMilkUI.new(moreMilk.settings,moreMilk.debug)
+		
+		uiSettingsmoreMilk:registerSettings()
+		
+		moreMilk.init = true
+	end
+end
+
+function moreMilk:loadAnimals()
+	if not self.isServer then return end
+
+	Logging.info("[%s]: Initializing mod v%s (c) 2025 by westor.", moreMilk.name, moreMilk.version)
+
+	moreMilk:loadSettings()
+
+	moreMilk:initCows()
+	moreMilk:initGoats()
+	
+	Logging.info("[%s]: End of mod initalization.", moreMilk.name)
+end
+
+function moreMilk:initCows()
+	for _1, subTypeIndex in ipairs(g_currentMission.animalSystem.nameToType["COW"].subTypes) do
+		local subType = g_currentMission.animalSystem.subTypes[subTypeIndex]
+
+		if subType.output.milk then
+			local fillType = subType.output.milk.fillType
+			local fillTypeName = g_fillTypeManager:getFillTypeNameByIndex(fillType)
+			local animalType = subType.name
+			
+			if fillType ~= nil and fillTypeName == "MILK" or fillTypeName == "BUFFALOMILK" then
+
+				for _2, output in ipairs(subType.output.milk.curve.keyframes) do
+					local amount = output[1]
+					local age = output.time
+
+					if amount ~= nil and amount ~= 0 and age ~= 0 then
+						local newAmount = amount * moreMilk.settings.Multiplier
+
+						output[1] = newAmount
+						
+						Logging.info("[%s]: Cow animal milk amount has been updated. - Animal Type: %s - Age: %s - Old Value: %s - New Value: %s - Multiplier: %s", moreMilk.name, animalType, age, amount, newAmount, moreMilk.settings.Multiplier)
+					end
+					
+				end
+				
+			end
+			
+		end
+		
+	end
+end
+
+function moreMilk:initGoats()
+	for _1, subTypeIndex in ipairs(g_currentMission.animalSystem.nameToType["SHEEP"].subTypes) do
+		local subType = g_currentMission.animalSystem.subTypes[subTypeIndex]
+
+		if subType.output.pallets then
+			local fillType = subType.output.pallets.fillType
+			local fillTypeName = g_fillTypeManager:getFillTypeNameByIndex(fillType)
+			local animalType = subType.name
+			
+			if fillType ~= nil and fillTypeName == "GOATMILK" then
+			
+				for _2, output in ipairs(subType.output.pallets.curve.keyframes) do
+					local amount = output[1]
+					local age = output.time
+
+					if amount ~= nil and amount ~= 0 and age ~= 0 then
+						local newAmount = amount * moreMilk.settings.Multiplier
+
+						output[1] = newAmount
+						
+						Logging.info("[%s]: Goat animal milk amount has been updated. - Animal Type: %s - Age: %s - Old Value: %s - New Value: %s - Multiplier: %s", moreMilk.name, animalType, age, amount, newAmount, moreMilk.settings.Multiplier)
+					end
+					
+				end
+				
+			end
+			
+		end
+		
+	end
+end
+
+AnimalSystem.loadAnimals = Utils.appendedFunction(AnimalSystem.loadAnimals, moreMilk.loadAnimals)
+
+addModEventListener(moreMilk)
---
+++ scripts/moreMilkUI.lua
@@ -0,0 +1,80 @@
+-- Author: westor
+-- Contact: westor7 @ Discord
+--
+-- Copyright (c) 2025 westor
+
+moreMilkUI = {}
+
+-- Create a meta table to get basic Class-like behavior
+local moreMilkUI_mt = Class(moreMilkUI)
+
+---Creates the settings UI object
+---@return SettingsUI @The new object
+function moreMilkUI.new(settings, debug)
+    local self = setmetatable({}, moreMilkUI_mt)
+
+    self.controls = {}
+	self.settings = settings
+	self.debug = debug
+
+    return self
+end
+
+---Register the UI into the base game UI
+function moreMilkUI:registerSettings()
+    -- Get a reference to the base game general settings page
+    local settingsPage = g_gui.screenControllers[InGameMenu].pageSettings
+	
+	-- Define the UI controls. For each control, a <prefix>_<name>_short and _long key must exist in the i18n values
+    local controlProperties = {
+        { name = "Multiplier", min = 1.5, max = 100, step = 0.5, autoBind = true, nillable = false }
+    }
+
+    UIHelper.createControlsDynamically(settingsPage, "mmi_setting_title", self, controlProperties, "mmi_")
+    UIHelper.setupAutoBindControls(self, self.settings, moreMilkUI.onSettingsChange)
+
+    -- Apply initial values
+    self:updateUiElements()
+
+    -- Update any additional settings whenever the frame gets opened
+    InGameMenuSettingsFrame.onFrameOpen = Utils.appendedFunction(InGameMenuSettingsFrame.onFrameOpen, function()
+        self:updateUiElements(true) -- We can skip autobind controls here since they are already registered to onFrameOpen
+    end)
+	
+	-- Trigger to update the values when settings frame is closed
+	InGameMenuSettingsFrame.onFrameClose = Utils.appendedFunction(InGameMenuSettingsFrame.onFrameClose, function()
+		self:onFrameClose();
+   	end);
+
+end
+
+function moreMilkUI:onSettingsChange()
+    self:updateUiElements()
+end
+
+---Updates the UI elements to reflect the current settings
+---@param skipAutoBindControls boolean|nil @True if controls with the autoBind properties shall not be newly populated
+function moreMilkUI:updateUiElements(skipAutoBindControls)
+    if not skipAutoBindControls then
+        -- Note: This method is created dynamically by UIHelper.setupAutoBindControls
+        self.populateAutoBindControls()
+    end
+
+	local isAdmin = g_currentMission:getIsServer() or g_currentMission.isMasterUser
+
+	for _, control in ipairs(self.controls) do
+		control:setDisabled(not isAdmin)
+	end
+	
+    -- Update the focus manager
+    local settingsPage = g_gui.screenControllers[InGameMenu].pageSettings
+    settingsPage.generalSettingsLayout:invalidateLayout()
+end
+
+function moreMilkUI:onFrameClose()
+	if moreMilk.settings.Multiplier == moreMilk.settings.Multiplier_OLD then return	end
+	
+	moreMilk.settings.Multiplier_OLD = moreMilk.settings.Multiplier
+
+	g_currentMission:showBlinkingWarning(g_i18n:getText("mmi_blink_warn"), 5000)
+end
